99adfa287754eca4afc7bfb73dbb4e8cb30aa5ff
Artyom <ligers2@mail.ru>
2023-05-12 15:01:54 +0500

    Mini-refactoring

--- Conference/Conference/Behavior/ValidationBehavior.cs
+++ Conference/Conference/Behavior/ValidationBehavior.cs
@@ -20,13 +20,15 @@ namespace Conference.Behavior
             if (_validators.Any())
             {
                 var context = new ValidationContext<TRequest>(request);
-                var validationResults = await Task.WhenAll(_validators
-                    .Select(v => v
-                        .ValidateAsync(context, cancellationToken)));
+                var validationResults = await Task
+                    .WhenAll(_validators
+                        .Select(v => v
+                            .ValidateAsync(context, cancellationToken)));
 
-                var failures = validationResults.SelectMany(r => r.Errors)
-                    .Where(f => f != null)
-                    .ToList();
+                var failures = validationResults
+                    .SelectMany(r => r.Errors)
+                        .Where(f => f != null)
+                        .ToList();
 
                 if (failures.Count != 0)
                 {
