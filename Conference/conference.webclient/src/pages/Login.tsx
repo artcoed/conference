6c60bba2d74536d9a188d2d7d4f4b184937101b1
Artyom <ligers2@mail.ru>
2023-05-15 13:14:57 +0500

    Add login page

--- Conference/conference.webclient/src/pages/Login.tsx
+++ Conference/conference.webclient/src/pages/Login.tsx
@@ -1,9 +1,21 @@
-import React, { FC } from 'react';
+import { Button, Form, Input } from 'antd';
+import React, { FC, useState } from 'react';
 
 const Login: FC = () => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submit = () => {
+    }
+
     return (
         <div>
             Login
+            <Form>
+                <Input placeholder="Login" value={login} onChange={e => setLogin(e.target.value)} />
+                <Input placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                <Button onClick={submit}>Login</Button>
+            </Form>
         </div>
     );
 };
