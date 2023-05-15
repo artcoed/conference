4dff216e8d1b48b585d4b2b402006cd6ba12c63a
Artyom <ligers2@mail.ru>
2023-05-15 13:52:00 +0500

    Create secretary meetings

--- Conference/conference.webclient/src/pages/Secretary/SecretaryMeeting.tsx
+++ Conference/conference.webclient/src/pages/Secretary/SecretaryMeeting.tsx
@@ -1,3 +1,4 @@
+import { Button, Form, Input } from 'antd';
 import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -8,9 +9,62 @@ type SecretaryMeetingParams = {
 const SecretaryMeeting: FC = () => {
     const { id } = useParams<SecretaryMeetingParams>();
 
+    const meeting = {
+        id: 2,
+        title: "Заголовок2",
+        startDateTime: Date.now(),
+        endDateTime: Date.now(),
+        hasCompleted: false,
+        votingTitle: "",
+        options: [],
+        hasVoting: false,
+        decisions: []
+    }
+
     return (
         <div>
-            SecretaryMeeting {id}
+            <h1>SecretaryMeeting {id}</h1>
+            <p>{meeting.title}</p>
+            <p>Start: {meeting.startDateTime}</p>
+            {meeting.hasCompleted && <p>End: {meeting.endDateTime}</p>}
+            <p>Voting:</p>
+            {meeting.hasVoting ? 
+                <>
+                    <p>{meeting.votingTitle}</p>
+                    <p>Options:</p>
+                    {meeting.options.map(option => 
+                        <p>{option}</p>
+                    )}
+                </>
+                : meeting.hasCompleted
+                    ?
+                    <p>Havent voting</p>
+                    :
+                    <>
+                        <Input placeholder="Title"/>
+                        <Input placeholder="Option" />
+                        <Button>Add option</Button>
+                        <Button>Add voting</Button>
+                    </>
+            }
+            <p>Decisions:</p>
+            {!meeting.hasCompleted &&
+                <Form>
+                    <Input placeholder="Decision"/>
+                    <Button>Add decision</Button>
+                </Form>
+            }
+            {meeting.decisions ?
+                <div>
+                    {meeting.decisions.map(decision =>
+                        <div>
+                            <p>{decision}</p>
+                        </div>
+                    )}
+                </div>
+                :
+                <p>Havent decisions</p>
+            }
         </div>
     );
 };
--- Conference/conference.webclient/src/pages/Secretary/SecretaryMeetings.tsx
+++ Conference/conference.webclient/src/pages/Secretary/SecretaryMeetings.tsx
@@ -1,17 +1,50 @@
-import { Button, Input } from 'antd';
-import React, { FC, useState } from 'react';
+import { Button, Form } from 'antd';
+import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GetMeetingPath } from '../../routes';
 
 const SecretaryMeetings: FC = () => {
     const navigate = useNavigate();
-    const [id, setId] = useState('0');
+
+    const meetings = [
+        {
+            id: 1,
+            title: "Заголовок",
+            startDateTime: Date.now(),
+            endDateTime: Date.now(),
+            hasCompleted: true
+        },
+        {
+            id: 2,
+            title: "Заголовок2",
+            startDateTime: Date.now(),
+            endDateTime: Date.now(),
+            hasCompleted: false
+        }
+    ]
 
     return (
         <div>
-            SecretaryMeetings
-            <Input value={id} onChange={(e) => setId(e.target.value)} />
-            <Button onClick={() => navigate(GetMeetingPath(id))}>Open meeting</Button>
+            <h1>SecretaryMeetings</h1>
+
+            <div>
+                {meetings.map(meeting =>
+                    <div>
+                        <p>{meeting.title}</p>
+                        <p>{meeting.startDateTime}</p>
+                        {meeting.hasCompleted ?
+                            <>
+                                <Button disabled>Meeting completed</Button>
+                            </>
+                            :
+                            <>
+                                <p>{meeting.endDateTime}</p>
+                                <Button onClick={() => navigate(GetMeetingPath(meeting.id.toString()))}>Open meeting</Button>
+                            </>
+                        }
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
